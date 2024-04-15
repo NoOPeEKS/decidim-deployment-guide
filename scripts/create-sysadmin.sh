fd65b4f75681a9517213e80ecc8efd76995ab436
Arnau <arnauapps@gmail.com>
2024-04-15 09:56:35 +0200

    Updated sysadmin creation script to accept parameters

--- scripts/create-sysadmin.sh
+++ scripts/create-sysadmin.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-`sudo docker exec container_decidim bundle exec rails runner "user = Decidim::System::Admin.new(email: 'system@example.org', password: 'decidim123456789', password_confirmation: 'decidim123456789'); user.save!;"`
+EMAIL=$1
+PASSWORD=$2
+
+sudo docker exec container_decidim bundle exec rails runner "user = Decidim::System::Admin.new(email: '$EMAIL', password: '$PASSWORD', password_confirmation: '$PASSWORD'); user.save!;"
