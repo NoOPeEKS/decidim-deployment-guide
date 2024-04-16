3d94fa26c018354f5820c57e1dea343316d1ec4e
Arnau Berenguer Jiménez <73296276+NoOPeEKS@users.noreply.github.com>
2024-04-16 17:48:12 +0200

    Comment update-orgadmin.sh

--- scripts/update-orgadmin.sh
+++ scripts/update-orgadmin.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# This script is only needed for previous versions such as 0.24.0. For actual version 0.28.0 it is not needed at all
 CONTAINERID=$1
 
 sudo docker exec $CONTAINERID bundle exec rails runner "user = Decidim::User.find_by(id: 2); user.admin = true; user.admin_terms_accepted_at = '2024-03-15 12:10:08'; user.save!;"
